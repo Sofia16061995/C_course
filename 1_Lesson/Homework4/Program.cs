91b2e71930617d3bdc198ea783707d6d12d7bd4b
Sofia <sofi.zoloto@yandex.ru>
2022-07-14 13:49:08 +0300

    Homework4

---
+++ 1_Lesson/Homework4/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Write number: ");
+ int a = int.Parse(Console.ReadLine());
+ if (a%2==0)
+     Console.WriteLine("Число четное");
+ else
+     Console.WriteLine("Число нечетное"); 
