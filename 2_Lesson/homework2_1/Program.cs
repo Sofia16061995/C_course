9f1ab89e0ca81def8772b4d0e48b64edfe7ca4b9
Sofia <sofi.zoloto@yandex.ru>
2022-07-19 11:35:10 +0300

    Homework 2_1

---
+++ 2_Lesson/homework2_1/Program.cs
@@ -0,0 +1,15 @@
+﻿int Method_1(int N)
+{
+   return N % 100 / 10;
+}
+Console.Write("Write a three-digit number N: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n > 999 ^ n < 100)
+{
+    Console.Write("Invalid value N");
+}
+else
+{
+    Console.WriteLine(Method_1(n));
+}
