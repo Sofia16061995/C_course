35fffb3c2f25a45b7afb84e1377eab9fb57b8907
Sofia <sofi.zoloto@yandex.ru>
2022-07-19 11:28:06 +0300

    Task 2_3

---
+++ 2_Lesson/2_3/Program.cs
@@ -0,0 +1,8 @@
+﻿void Checking (int a, int b)
+{
+    if (a % b == 0)
+    Console.WriteLine($"{a} multiple {b}");
+    else
+    Console.WriteLine($"{a} not multiple {b}, remainder of the division {a % b}");
+}
+Checking (15,4);
