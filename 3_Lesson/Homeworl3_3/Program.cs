8431b482b7640f93816358881483d414843369a7
Sofia <sofi.zoloto@yandex.ru>
2022-07-22 07:57:49 +0300

    Homework3_3

---
+++ 3_Lesson/Homeworl3_3/Program.cs
@@ -0,0 +1,13 @@
+﻿void CubeNumbers (int n)
+ {
+     int i = 1;
+     while (i <= n)
+     {
+         Console.WriteLine(Math.Pow(i, 3));
+         i += 1;
+     }
+ }
+
+ Console.Write("Write a number N: ");
+ int n = int.Parse(Console.ReadLine());
+ CubeNumbers(n);
