c8cf1cad10f74e734917e2a584a501be8e38f62a
Sofia <sofi.zoloto@yandex.ru>
2022-07-22 07:54:58 +0300

    Homework3_1_1

---
+++ 3_Lesson/Homework3_1_1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Write a five-digit number N: ");
+ string n = Console.ReadLine();
+
+ if (n.Length != 5)
+ {
+     Console.WriteLine("You didn't write a five-digit number");
+ }
+ else
+ {
+     if (n[0] == n[4] && n[1] == n[3])
+     {
+        Console.WriteLine("The number is a palindrome");
+     }
+     else
+     {
+         Console.WriteLine("The number is not a palindrome");
+     }
+ }
