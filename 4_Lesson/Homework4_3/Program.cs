fc81722328fca330ea9d58c57e20c97ba3850fba
Sofia <sofi.zoloto@yandex.ru>
2022-08-06 13:17:48 +0300

    Homework 4_3

---
+++ 4_Lesson/Homework4_3/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] arr(int num)
+{
+    int[] array = new int[num];
+    for(int i=0; i < num; i++)
+    {
+        array[i]=new Random().Next(0, 10);
+    }
+    return array;
+}
+arr(8);
+
+void PrintArray (int[] array2)
+{
+    for(int i=0; i < array2.Length; i++)
+        Console.WriteLine($"{array2[i]} ");
+}
+PrintArray(arr(8));
