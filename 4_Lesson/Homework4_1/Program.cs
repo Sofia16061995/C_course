7a2f7d2ace46ed90323b5cf7571cc0e26d03b783
Sofia <sofi.zoloto@yandex.ru>
2022-08-06 13:09:38 +0300

    Homework 4_1

---
+++ 4_Lesson/Homework4_1/Program.cs
@@ -0,0 +1,12 @@
+﻿int Nums (int a, int b)
+
+ {
+     int exponentiation = a;
+     for(int i = 1; i<b; i++)
+     exponentiation = exponentiation * a;
+     return exponentiation;
+ }
+
+ Console.WriteLine(Nums(3, 5));
+ Console.WriteLine(Nums(2, 4));
+
