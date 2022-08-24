af24a43c39701e6041848c86173db572a007b3ff
Sofia <sofi.zoloto@yandex.ru>
2022-08-24 21:42:29 +0300

    Homework 9_2

---
+++ 9_Lesson/Homework9_2/Program.cs
@@ -0,0 +1,14 @@
+﻿void SumNum (int m, int n, int sum)
+{
+    if (n >= m) 
+    {
+        sum = sum + (m++);
+        SumNum(m, n, sum);
+        return;
+    }
+    Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+}
+
+SumNum(1, 15, 0);
+SumNum(4, 8, 0);
+
